abd17319abe9c428abe0e57fdaa7cf3bd4230490
kalyanofficial980-art <kalyanofficial980@gmail.com>
2026-08-20 08:57:01 +0000

    Remove stale trigger migration timestamp

--- supabase/migrations/20260820072500_revoke_trigger_function_execute.sql
+++
@@ -1,9 +0,0 @@
-begin;
-
--- These SECURITY DEFINER functions are trigger entrypoints only. They do not
--- need to be callable through PostgREST/RPC by anon or authenticated users.
-revoke execute on function public.enforce_website_plan_resource_limits_v1() from public, anon, authenticated;
-revoke execute on function public.prevent_admin_customer_website_write_v1() from public, anon, authenticated;
-revoke execute on function public.prevent_admin_scan_quota_v1() from public, anon, authenticated;
-
-commit;
